a287c3fe199f89b6955426774b10271c0b308ab3
Jean Rodriguez <rodriguezzegarrajeanpierre@gmail.com>
2025-11-08 09:34:42 -0300

    Ajustes de conexion .env y config/Database.php.

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'hostname' => 'api_platia_db',         // o 'localhost' si corre fuera del contenedor
+        'username' => 'root',
+        'password' => 'root',
+        'database' => 'api_platia_db',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
